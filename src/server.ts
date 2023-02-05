434b8cf0138b97a387b63a80d4c6c2b133a7c860
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-05 16:11:24 +0300

    feat: set up eslint
    - intall eslint;
    - set up eslint config;

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running");
+	console.log('Server is running');
 });
