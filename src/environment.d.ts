a9377d0d61d1f32fa3ad7cbd3393d454e5b895c2
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-07 20:56:27 +0300

    feat: declare .env vars types

---
+++ src/environment.d.ts
@@ -0,0 +1,16 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: number;
+      MONGO_DB_URL: string;
+      JWT_ACCESS_SECRET: string;
+      JWT_REFRESH_SECRET: string;
+      API_URL: string;
+      CLIENT_URL: string;
+      GMAIL_USER: string;
+      GMAIL_PASSWORD: string;
+    }
+  }
+}
