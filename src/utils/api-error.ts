50e17ec18f39199725129d65653f5f44f1130cc2
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-07 21:54:56 +0300

    feat: implement api-error util

--- src/utils/api-error.ts
+++ src/utils/api-error.ts
@@ -1,24 +1,24 @@
-import {INTERNAL_SERVER_ERROR} from './constants';
-
-type ApiErrorPropsType = {
-	message: string;
-	errors: string[];
-	status: number;
-};
+import { type ValidationError } from 'express-validator';
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from '../constants/status-codes-const';
 
 class ApiError extends Error {
-	errors: string[];
-	status: number;
+  errors: ValidationError[];
 
-	constructor({
-		message,
-		errors = [],
-		status = INTERNAL_SERVER_ERROR,
-	}: ApiErrorPropsType) {
-		super(message);
-		this.errors = errors;
-		this.status = status;
-	}
+  status: number;
+
+  constructor(
+    message: string,
+    status: number = INTERNAL_SERVER_ERROR,
+    errors: ValidationError[] = [],
+  ) {
+    super(message);
+    this.errors = errors;
+    this.status = status;
+  }
+
+  static badRequest(message: string, errors: ValidationError[] = []) {
+    return new ApiError(message, BAD_REQUEST, errors);
+  }
 }
 
 export default ApiError;
