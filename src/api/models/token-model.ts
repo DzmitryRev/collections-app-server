e2e82b49ca9e58bd8f5c75dc5bbe9200032fe2f7
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-07 21:56:17 +0300

    feat: implement token model

--- src/api/models/token-model.ts
+++ src/api/models/token-model.ts
@@ -1,14 +1,14 @@
-import {Schema, model} from 'mongoose';
-import {type UserType} from './user-model';
+import { Schema, model } from 'mongoose';
+import { type UserType } from './user-model';
 
 export type TokenType = {
-	user: UserType;
-	refreshToken: string;
+  user: UserType;
+  refreshToken: string;
 };
 
 const tokenSchema = new Schema<TokenType>({
-	user: {type: Schema.Types.ObjectId, ref: 'User'},
-	refreshToken: {type: String, required: true},
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  refreshToken: { type: String, required: true },
 });
 
 export default model('Token', tokenSchema);
