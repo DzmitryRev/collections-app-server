f7a60014ff5efbec91d53c03a15d1a819bd1ce5f
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-07 22:50:20 +0300

    feat: implement error-middleware

--- src/middlewares/error-middleware.ts
+++ src/middlewares/error-middleware.ts
@@ -1,13 +1,14 @@
-import {type NextFunction, type Request, type Response} from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { type NextFunction, type Request, type Response } from 'express';
+import { INTERNAL_SERVER_ERROR } from '../constants/status-codes-const';
 import ApiError from '../utils/api-error';
-import {INTERNAL_SERVER_ERROR} from '../utils/constants';
 
 function errorMiddleware(err: Error, req: Request, res: Response, next: NextFunction) {
-	if (err instanceof ApiError) {
-		return res.status(err.status).json({message: err.message, errors: err.errors});
-	}
+  if (err instanceof ApiError) {
+    return res.status(err.status).json({ message: err.message, errors: err.errors });
+  }
 
-	return res.status(INTERNAL_SERVER_ERROR).json({message: 'Непредвиденная ошибка'});
+  return res.status(INTERNAL_SERVER_ERROR).json({ message: 'Unknown error' });
 }
 
 export default errorMiddleware;
