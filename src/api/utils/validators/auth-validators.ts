790ef4b829a4789e24157b2e3c6602d2f9c839c4
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-11 19:34:06 +0300

    feat: complete auth validators (without reset password)

--- src/api/utils/validators/auth-validators.ts
+++ src/api/utils/validators/auth-validators.ts
@@ -1,35 +1,56 @@
-import { check } from 'express-validator';
+import { NextFunction, Request, Response } from 'express';
+import { check, validationResult } from 'express-validator';
+import { VALIDATION_ERROR } from '../../../constants/errors-messages-const';
 import {
   EMAIL_IS_NOT_VALID,
-  IS_EMPTY,
+  EMAIL_IS_EMPTY,
+  NAME_IS_EMPTY,
+  PASSWORD_IS_EMPTY,
   PASSWORD_TOO_SHORT_MIN_6,
 } from '../../../constants/validation-const';
+import ApiError from '../../../utils/api-error';
+
+const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (errors && !errors.isEmpty()) {
+    next(ApiError.badRequest(VALIDATION_ERROR, errors.array()));
+  }
+  next();
+};
 
 export const validateLogin = [
-  check('email').not().isEmpty().withMessage(IS_EMPTY)
+  check('email')
+    .not()
+    .isEmpty()
+    .withMessage(EMAIL_IS_EMPTY)
     .isEmail()
     .withMessage(EMAIL_IS_NOT_VALID),
   check('password')
     .not()
     .isEmpty()
-    .withMessage(IS_EMPTY)
+    .withMessage(PASSWORD_IS_EMPTY)
     .isLength({
       min: 5,
     })
     .withMessage(PASSWORD_TOO_SHORT_MIN_6),
+  validateRequest,
 ];
 
 export const validateRegister = [
-  check('name').not().isEmpty().withMessage(IS_EMPTY),
-  check('email').not().isEmpty().withMessage(IS_EMPTY)
+  check('name').not().isEmpty().withMessage(NAME_IS_EMPTY),
+  check('email')
+    .not()
+    .isEmpty()
+    .withMessage(EMAIL_IS_EMPTY)
     .isEmail()
     .withMessage(EMAIL_IS_NOT_VALID),
   check('password')
     .not()
     .isEmpty()
-    .withMessage(IS_EMPTY)
+    .withMessage(PASSWORD_IS_EMPTY)
     .isLength({
-      min: 5,
+      min: 6,
     })
     .withMessage(PASSWORD_TOO_SHORT_MIN_6),
+  validateRequest,
 ];
